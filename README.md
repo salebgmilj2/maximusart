f11f4010150787c5ccfaa2e7a36f8958d00652f4
salebgmilj2 <aleksandar.salebg@gmail.com>
2014-01-30 12:47:44 +0100

    Inicijalni commit

---
+++ README.md
@@ -0,0 +1,4 @@
+maximusart
+==========
+
+Maximus Art
---
+++ js/parallaxJS.js
@@ -0,0 +1,16 @@
+
+var $window = $(window);
+var velocity = 0.4;
+
+function update(){
+	var pos = $window.scrollTop();
+	
+	$('.container').each(function() {
+		var $element = $(this);
+		var height = $element.height();
+
+		$(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px'); 
+	}); 
+};
+
+$window.bind('scroll', update);
